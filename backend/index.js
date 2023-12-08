68850458482404580fa61cb63a343c51a03b47ba
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-08 11:57:39 +0530

    Gaurav

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
-// HELLO WORLD
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
--- backend/models/courses.js
+++ backend/models/courses.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    admission_no: String,
+const CoursesSchema = new Schema({
     email: {
         type: String,
         required: true,
         unique: true
-    }
+    },
+    name: String,
+    prof: String
 })
 
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Courses', CoursesSchema);
