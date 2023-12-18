c255680fb4937777490bad3f732a0279fc19ec66
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-18 11:38:54 +0530

    comment and post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -39,6 +39,8 @@ module.exports.deletePost = async (req, res) => {
     try {
         const { id } = req.params;
         await Post.findByIdAndDelete(id);
+        res.status(200).json({ message: "Successfully deleted" });
+
     }
     catch (e) {
         res.status(401).json({ name: e.name, message: e.message });
@@ -48,6 +50,8 @@ module.exports.editPost = async (req, res) => {
     try {
         const { id } = req.params;
         await Post.findByIdAndUpdate(id, { ...req.body.post });
+        res.status(200).json({ message: "Successfully edited" });
+
     }
     catch (e) {
         res.status(401).json({ name: e.name, message: e.message });
