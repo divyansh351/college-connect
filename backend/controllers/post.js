fc464849f39707787a8e6444dc2a1e6ac640605e
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-18 11:37:01 +0530

    comment and post

---
+++ backend/controllers/post.js
@@ -0,0 +1,55 @@
+const Course = require("../models/course")
+const Post = require("../models/post")
+
+module.exports.addPost = async (req, res) => {
+    try {
+        const {
+            course_id,
+            user_id,
+            title,
+            content
+        } = req.body;
+        const course = await Course.findById(course_id);
+        const new_post = {
+            title: title,
+            content: content,
+            associated_course: course_id,
+            uploader: user_id
+        }
+        course.associated_posts.push(new_post);
+        await course.save();
+        const post = new Post(new_post);
+        await post.save();
+        res.status(200).json({ message: "successfully posted" })
+    }
+    catch (e) {
+        res.status(500).json({ message: e.message, name: e.name });
+    }
+}
+module.exports.viewPost = async (req, res) => {
+    try {
+        const postList = await Post.find({});
+        res.status(200).json(postList);
+    }
+    catch (e) {
+        res.status(500).json({ message: e.message, name: e.name });
+    }
+}
+module.exports.deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Post.findByIdAndDelete(id);
+    }
+    catch (e) {
+        res.status(401).json({ name: e.name, message: e.message });
+    }
+}
+module.exports.editPost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Post.findByIdAndUpdate(id, { ...req.body.post });
+    }
+    catch (e) {
+        res.status(401).json({ name: e.name, message: e.message });
+    }
+}
