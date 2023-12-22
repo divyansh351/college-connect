53d09557b7c6453e7bd4c072eb8551ad136f7929
Divyansh Gupta <94603079+divyansh351@users.noreply.github.com>
2023-12-22 16:23:08 +0530

    Update post.js

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -8,7 +8,7 @@ router.route("/new")
     .post(checkToken, Post.addPost)
 
 router.route("/:id/comments")
-    .get(Post.viewComments)// single coures view
+    .post(Post.viewComments)// single coures view
 
 router.route("/:id/delete")
     .delete(Post.deletePost)
